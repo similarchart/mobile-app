f1c367b237ed54167ce5f55dfac8443c77aa1144
leechanhoe <klavier0823@naver.com>
2024-03-14 15:22:23 +0900

    상단 앱바 없애고 하단 네비게이션 바 UI만 생성

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,31 +5,53 @@ final homeUrl = Uri.parse('https://www.similarchart.com');
 
 class HomeScreen extends StatelessWidget {
   WebViewController controller = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    // ..setJavaScriptMode(JavaScriptMode.unrestricted)
     ..loadRequest(homeUrl);
 
   HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.grey,
-        title: Text('similar chart'),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {
-              controller.loadRequest(homeUrl);
-            },
-            icon: Icon(
-              Icons.home,
-            ),
+    return SafeArea( // SafeArea 추가
+      child: Scaffold(
+        bottomNavigationBar: BottomAppBar(
+          color: Color(0xFF343a40), // 배경색 설정
+          child: Row(
+            children: <Widget>[
+              buildBottomIcon(Icons.home, '홈', () {
+                controller.loadRequest(homeUrl);
+              }),
+              buildBottomIcon(Icons.star , '즐겨찾기', () {
+                // 즐겨찾기 기능
+              }),
+              buildBottomIcon(Icons.history, '방문기록', () {
+                // 방문기록 기능
+              }),
+              buildBottomIcon(Icons.settings, '설정', () {
+                // 설정 기능
+              }),
+            ],
           ),
-        ],
+        ),
+        body: WebViewWidget(
+          controller: controller,
+        ),
       ),
-      body: WebViewWidget(
-        controller: controller,
+    );
+  }
+
+  Widget buildBottomIcon(IconData icon, String label, VoidCallback onTap) {
+    return Expanded(
+      child: InkWell(
+        onTap: onTap,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(icon, color: Colors.white), // 아이콘 색상 설정
+            Text(label, style: TextStyle(fontSize: 12, color: Colors.white)), // 텍스트 색상 및 스타일 설정
+          ],
+        ),
       ),
     );
   }
