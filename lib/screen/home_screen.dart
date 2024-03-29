ff75287776dcfee94c11e8c7b3b9c270db4b7536
mssak <cxz017@naver.com>
2024-03-29 17:33:08 +0900

    설정 에 있는 '앱 시작시 기본언어' =>'언어' 로 기능변경, 적용 버튼 추가(버튼 누를시 홈스크린 refresh)

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -11,7 +11,8 @@ class HomeScreen extends StatelessWidget {
   final WebViewController controller = WebViewController();
 
   Future<void> loadInitialUrl(WebViewController controller) async {
-    String lang = await LanguagePreference.getLanguageSetting(); // 현재 설정된 언어를 불러옵니다.
+    String lang =
+        await LanguagePreference.getLanguageSetting(); // 현재 설정된 언어를 불러옵니다.
     Uri homeUrl = Uri.parse('https://www.similarchart.com?lang=$lang');
     controller
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
@@ -35,12 +36,8 @@ class HomeScreen extends StatelessWidget {
                 buildBottomIcon(Icons.home, '홈', () => onHomeTap(controller)),
                 buildBottomIcon(Icons.star, '즐겨찾기', () => {}),
                 buildBottomIcon(Icons.history, '방문기록', () => {}),
-                buildBottomIcon(Icons.settings, '설정', () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SettingsScreen()),
-                  );
-                }),
+                buildBottomIcon(
+                    Icons.settings, '설정', () => onSettingsTap(context)),
               ],
             ),
           ),
@@ -52,7 +49,6 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-
   Widget buildBottomIcon(IconData icon, String label, VoidCallback onTap) {
     return Expanded(
       child: InkWell(
@@ -62,10 +58,40 @@ class HomeScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Icon(icon, color: Colors.white), // 아이콘 색상 설정
-            Text(label, style: TextStyle(fontSize: 12, color: Colors.white)), // 텍스트 색상 및 스타일 설정
+            Text(label,
+                style: TextStyle(
+                    fontSize: 12, color: Colors.white)), // 텍스트 색상 및 스타일 설정
           ],
         ),
       ),
     );
+  }
+
+// '설정' 버튼 탭 처리를 위한 별도의 함수
+  Future<void> onSettingsTap(BuildContext context) async {
+    final doRefresh = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SettingsScreen()),
+    );
+
+    // 설정 화면에서 돌아온 후 반환된 데이터에 따라 필요한 작업 수행
+    if (doRefresh != null && doRefresh) {
+      // 현재 웹뷰의 URL을 가져옵니다.
+      String? currentUrl = await controller.currentUrl();
+      Uri currentUri = Uri.parse(currentUrl ?? "");
+
+      // 선호하는 언어 설정을 가져옵니다.
+      String preferLang = await LanguagePreference.getLanguageSetting();
+
+      // 현재 URI의 쿼리 매개변수를 변경하되, lang 매개변수만 새로운 값으로 설정합니다.
+      Map<String, String> newQueryParameters = Map.from(currentUri.queryParameters);
+      newQueryParameters['lang'] = preferLang; // lang 매개변수 업데이트
+
+     // 변경된 쿼리 매개변수를 포함하여 새로운 URI 생성
+      Uri newUri = currentUri.replace(queryParameters: newQueryParameters);
+
+      // 새로운 URI로 웹뷰를 로드합니다.
+      controller.loadRequest(newUri);
+    }
   }
 }
--- lib/screen/settings_screen.dart
+++ lib/screen/settings_screen.dart
@@ -31,34 +31,60 @@ class _SettingsScreenState extends State<SettingsScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
           children: [
-            Text(
-              '앱 시작시 기본언어',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: AppColors.textColor,
-              ),
-            ),
-            Spacer(), // 텍스트와 드롭다운 사이에 공간을 추가합니다.
-            DropdownButton<String>(
-              // 드롭다운 버튼의 크기를 강제로 제한하지 않고 내용에 맞게 조절합니다.
-              value: _selectedLanguage,
-              dropdownColor: AppColors.secondaryColor,
-              items: [
-                DropdownMenuItem(value: 'ko', child: Text('한국어', style: TextStyle(color: AppColors.textColor))),
-                DropdownMenuItem(value: 'en', child: Text('English', style: TextStyle(color: AppColors.textColor))),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '언어',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.textColor,
+                  ),
+                ),
+                Spacer(), // 텍스트와 드롭다운 사이에 공간을 추가합니다.
+                DropdownButton<String>(
+                  // 드롭다운 버튼의 크기를 강제로 제한하지 않고 내용에 맞게 조절합니다.
+                  value: _selectedLanguage,
+                  dropdownColor: AppColors.secondaryColor,
+                  items: [
+                    DropdownMenuItem(
+                        value: 'ko',
+                        child: Text('한국어',
+                            style: TextStyle(color: AppColors.textColor))),
+                    DropdownMenuItem(
+                        value: 'en',
+                        child: Text('English',
+                            style: TextStyle(color: AppColors.textColor))),
+                  ],
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedLanguage = value;
+                      LanguagePreference.setLanguageSetting(value!);
+                    });
+                  },
+                  style: TextStyle(color: AppColors.textColor),
+                  underline: Container(height: 2, color: AppColors.textColor),
+                ),
               ],
-              onChanged: (value) {
-                setState(() {
-                  _selectedLanguage = value;
-                  LanguagePreference.setLanguageSetting(value!);
-                });
+            ),
+            Spacer(),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context, true);
               },
-              style: TextStyle(color: AppColors.textColor),
-              underline: Container(height: 2, color: AppColors.textColor),
+              child: Text('적용'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.primaryColor, // 버튼의 배경색 설정
+                foregroundColor: AppColors.textColor, // 버튼의 텍스트 색상 설정
+                elevation: 5, // 버튼의 그림자 정도
+                shape: RoundedRectangleBorder( // 버튼의 모양 정의
+                  borderRadius: BorderRadius.circular(10), // 모서리를 둥글게
+                  side: BorderSide(color: AppColors.secondaryColor, width: 2), // 테두리 색상과 두께
+                ),
+              ),
             ),
           ],
         ),
