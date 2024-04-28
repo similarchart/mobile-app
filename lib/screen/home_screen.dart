0f8ba8dbd3af1ce7b2cb96417e34b065421ee887
mssak <cxz017@naver.com>
2024-04-29 02:18:40 +0900

    홈버튼 로딩안되는 오류 수정 및 button/home삭제후 home_screen에 통합

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -6,7 +6,6 @@ import 'package:hive/hive.dart';
 import 'package:web_view/screen/favorite_screen.dart';
 import 'package:web_view/services/toast_service.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:web_view/button/home.dart';
 import 'package:web_view/services/language_preference.dart';
 import 'package:web_view/screen/settings_screen.dart';
 import 'package:web_view/screen/splash_screen.dart';
@@ -228,8 +227,8 @@ class _HomeScreenState extends State<HomeScreen> {
               mainAxisSize: MainAxisSize.max,
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Icon(icon, color: Colors.white, size: 20,), // 아이콘 색상 및 크기 설정
-                Text(label, style: TextStyle(fontSize: 9, color: Colors.white)), // 텍스트 스타일 설정
+                Icon(icon, color: Colors.white, size: 25,), // 아이콘 색상 및 크기 설정
+                Text(label, style: TextStyle(fontSize: 12, color: Colors.white)), // 텍스트 스타일 설정
               ],
             ),
           ),
@@ -349,6 +348,25 @@ class _HomeScreenState extends State<HomeScreen> {
       });
       controller.loadRequest(Uri.parse(url));
     }
+  }
+
+  void onHomeTap(WebViewController controller) async {
+    // 현재 웹뷰의 URL을 가져옵니다.
+    String? currentUrl = await controller.currentUrl();
+    Uri uri = Uri.parse(currentUrl ?? "");
+
+    String prefer_lang = await LanguagePreference.getLanguageSetting();
+    // 현재 URL에서 언어 쿼리 매개변수(lang)를 확인합니다.
+    String lang = uri.queryParameters['lang'] ?? prefer_lang; // 기본값은 'ko'
+
+    // 새로운 홈 URL을 만들되, 현재 언어 설정을 유지합니다.
+    Uri newHomeUrl = Uri.parse('https://www.similarchart.com?lang=$lang');
+
+    // 새로운 홈 URL로 페이지를 로드합니다.
+    setState(() {
+      _isLoading = true;
+    });
+    controller.loadRequest(newHomeUrl);
   }
 
   // '설정' 버튼 탭 처리를 위한 별도의 함수
